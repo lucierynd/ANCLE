574b9d711b35cd26f92c77320e17299f9476c77d
lucierynd <reynaudlucie83@gmail.com>
2025-12-19 15:47:37 -0500

    Convert simulation into normal directory

---
+++ src/auv_simulation_pkg/launch/sim.launch.py
@@ -0,0 +1,174 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import (
+    IncludeLaunchDescription,
+    TimerAction,
+    DeclareLaunchArgument
+)
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+package_name = "auv_simulation_pkg"
+
+
+def generate_static_tf_publisher_node(parentFrame, childFrame):
+    return Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        name=f"static_tf_publisher_{parentFrame}",
+        arguments=["0", "0", "0", "0", "0", "0", parentFrame, childFrame],
+        parameters=[{"use_sim_time": True}],
+    )
+
+
+def generate_launch_description():
+    pkg_ros_gz_sim = get_package_share_directory("ros_gz_sim")
+    pkg_path = get_package_share_directory(package_name)
+
+    rviz_config_file = LaunchConfiguration("rviz_config")
+    rviz_config_arg = DeclareLaunchArgument(
+        "rviz_config",
+        default_value=os.path.join(pkg_path, "rviz", "basic.rviz"),
+        description="Path to rviz config file",
+    )
+
+    model = os.path.join(pkg_path, "models", "tethys_rviz", "model.sdf")
+
+    with open(model, "r") as infp:
+        robot_desc = infp.read().replace(
+            "<uri>", f"<uri>package://{package_name}/models/tethys_rviz/"
+        )
+
+    # Gazebo launch
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_ros_gz_sim, "launch", "gz_sim.launch.py"),
+        ),
+        launch_arguments={
+            "gz_args": f"-r \"/ros2_ws/src/auv_simulation_pkg/models/pool/pool_party.sdf\""
+        }.items(),
+    )
+
+    rviz = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        arguments=["-d", rviz_config_file],
+    )
+
+    bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        name="tethys_bridge",
+        parameters=[{"use_sim_time": True}],
+        arguments=[
+            "/world/world_demo/model/tethys/link/camera_front/sensor/color/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo",
+            "/world/world_demo/model/tethys/link/camera_front/sensor/color/image@sensor_msgs/msg/Image[gz.msgs.Image",
+            "/world/world_demo/model/tethys/link/camera_front/sensor/depth/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo",
+            "/world/world_demo/model/tethys/link/camera_front/sensor/depth/depth_image@sensor_msgs/msg/Image[gz.msgs.Image",
+            "/world/world_demo/model/tethys/link/camera_front/sensor/depth/depth_image/points@sensor_msgs/msg/PointCloud2[gz.msgs.PointCloudPacked",
+            "/world/world_demo/model/tethys/link/imu_link/sensor/imu/imu@sensor_msgs/msg/Imu[gz.msgs.IMU",
+            "/model/tethys/odometry@nav_msgs/msg/Odometry[gz.msgs.Odometry",
+            "/model/tethys/cmd_vel@geometry_msgs/msg/Twist]gz.msgs.Twist",
+            "/world/world_demo/model/tethys/link/scan_omni/sensor/scan_omni/scan@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan",
+            "/world/world_demo/model/tethys/joint_state@sensor_msgs/msg/JointState[gz.msgs.Model",
+            "/model/tethys/tf@tf2_msgs/msg/TFMessage[gz.msgs.Pose_V",
+            "/model/tethys/pose@geometry_msgs/msg/TransformStamped[gz.msgs.Pose",
+            "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock"
+        ],
+        remappings=[
+            (
+                "/world/world_demo/model/tethys/link/camera_front/sensor/color/camera_info",
+                "camera_front/camera_info",
+            ),
+            (
+                "/world/world_demo/model/tethys/link/camera_front/sensor/color/image",
+                "camera_front/image",
+            ),
+            (
+                "/world/world_demo/model/tethys/link/camera_front/sensor/depth/camera_info",
+                "camera_front/depth/camera_info",
+            ),
+            (
+                "/world/world_demo/model/tethys/link/camera_front/sensor/depth/depth_image",
+                "camera_front/depth/image",
+            ),
+            (
+                "/world/world_demo/model/tethys/link/camera_front/sensor/depth/depth_image/points",
+                "scan_3D",
+            ),
+            ("/model/tethys/odometry", "odom"),
+            ("/model/tethys/cmd_vel", "cmd_vel"),
+            (
+                "/world/world_demo/model/tethys/link/imu_link/sensor/imu/imu",
+                "imu/data",
+            ),
+            (
+                "/world/world_demo/model/tethys/link/scan_omni/sensor/scan_omni/scan",
+                "scan",
+            ),
+            ("/world/world_demo/model/tethys/joint_state", "joint_states"),
+        ],
+        output="screen",
+    )
+
+    delayedNodes = TimerAction(
+        period=2.5,
+        actions=[rviz, bridge],
+    )
+
+    robotState = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="both",
+        parameters=[
+            {"use_sim_time": True},
+            {"robot_description": robot_desc},
+        ],
+    )
+
+    # RF2O Node - Lidar only odometry
+    # rf2o = IncludeLaunchDescription(
+    #             PythonLaunchDescriptionSource([os.path.join(
+    #                 get_package_share_directory('rf2o_laser_odometry'),'launch','rf2o_laser_odometry.launch.py')])
+    # )
+
+    # IMU + lidar odometry without TF publishing
+    # RF2O Node - Lidar only odometry without TF publishing
+    rf2o = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('auv_simulation_pkg'),'launch','rf2o_no_tf_launch.py')])
+    )
+
+    # EKF Node
+    ekf = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('auv_simulation_pkg'),'launch','ekf_launch.py')])
+    )
+
+    return LaunchDescription(
+        [
+            rviz_config_arg,
+            gazebo,
+            robotState,
+            delayedNodes,
+            generate_static_tf_publisher_node(
+                "scan_omni", "tethys/scan_omni/scan_omni"
+            ),
+            generate_static_tf_publisher_node(
+                "camera_front", "tethys/camera_front/color"
+            ),
+            generate_static_tf_publisher_node(
+                "camera_front", "tethys/camera_front/depth"
+            ),
+            generate_static_tf_publisher_node(
+                "imu_link", "tethys/imu_link/imu"
+            ),
+            rf2o,
+            ekf
+        ]
+    )
---
+++ src/auv_simulation_pkg/launch/slam.launch.py
@@ -0,0 +1,19 @@
+import os
+
+from launch.actions import IncludeLaunchDescription
+from launch import LaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    pkg_path = get_package_share_directory("auv_simulation_pkg")
+
+    slam_toolbox = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('slam_toolbox'),'launch','online_async_launch.py')]),
+                launch_arguments={'slam_params_file': os.path.join(
+                    get_package_share_directory('ancle_pkg'), 'config', 'slam_toolbox_rplidar_config.yaml')}.items()
+    )
+
+    return LaunchDescription([slam_toolbox])
